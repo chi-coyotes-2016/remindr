fea0ecf4aa4ca60401c9ef95486be5b36e93fe9c
busster <jasonmbuss@gmail.com>
2016-11-17 16:17:11 -0600

    add groups and show groups

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,37 @@
+class GroupsController < ApplicationController
+
+  def index
+    @user = User.find_by(id: current_user.id)
+    if current_user
+      @groups = @user.groups
+    end
+  end
+
+  def show
+    @user = current_user
+    @group = @user.groups.find_by(id: params[:id])
+  end
+
+  def new
+    @user = User.find_by(id: current_user.id)
+  end
+
+  def create
+    if current_user
+      @group = Group.new(group_params)
+      current_user.groups << @group
+      if @group.save
+        redirect_to user_group_path(@group.user, @group)
+      else
+        render :new
+      end
+    end
+  end
+
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
+
+end
