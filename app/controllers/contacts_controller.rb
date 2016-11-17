55ca67e8505af6d1bc8766a4b4ddfc34a58f21f9
Jsumulong <sumulong28@gmail.com>
2016-11-17 16:05:08 -0600

    add page to create new contact, view and controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,34 @@
 class ContactsController < ApplicationController
 
-def index
-  @user = User.find_by(id: current_user.id)
-  if logged_in?
-    if current_user.id == session[:user_id]
+	def index
+	  @user = User.find_by(id: current_user.id)
+	  if logged_in?
+	    if current_user.id == session[:user_id]
 
-    end
-  end
-end
+	    end
+	  end
+	end
+
+	def new
+		@user = User.find(params[:user_id])
+		@contact = Contact.new
+	end
+
+	def create
+		@user = User.find(params[:user_id])
+		@contact = Contact.new(contact_params)
+		@contact.user_id = @user.id
+		if @contact.save
+			redirect_to user_contact_path(@user, @contact)
+		else
+			@errors = @contact.errors.full_messages
+			render 'new'
+		end
+	end
+
+private
+	def contact_params
+		params.require(:contact).permit(:name, :phone_number)
+	end
 
 end
