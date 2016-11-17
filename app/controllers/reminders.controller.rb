d0ecc96f288ad746a029b5a204733b661d554dd7
busster <jasonmbuss@gmail.com>
2016-11-17 11:48:25 -0600

    populating reminders page with remidners

--- app/controllers/reminders.controller.rb
+++ app/controllers/reminders.controller.rb
@@ -3,5 +3,12 @@ class SessionsController < ApplicationController
   def new
 
   end
+
+  def show
+    if logged_in?
+      @user - User.find_by(id: session[:user_id])
+      @reminders = @user.reminders.order(time_to_go_out: :desc)
+    end
+  end
   
 end
