dc17739339c717e354af84e4ae175dbf92debd24
Jsumulong <sumulong28@gmail.com>
2016-11-17 12:37:50 -0600

    add form and test for register new user page, add styles to form

--- spec/features/registering_a_new_user_spec.rb
+++ spec/features/registering_a_new_user_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 	feature "registering a new user" do
+		
 		scenario "the user sees a registration form" do 
 			visit "/users/new"
-			expect(page).to have_field(first_name, :type => 'text')
+			expect(page).to have_field("user[first_name]") 
+			# response.should have_field("first_name")
 		end
+
+		xscenario "" do 
+		end
+
 	end 
