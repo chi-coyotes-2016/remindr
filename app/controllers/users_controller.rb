c1a69b634b1d554ec0a522a147ee9b892d7e3c92
Sam Hopkins <hopkins.571@gmail.com>
2016-11-17 14:39:59 -0600

    add full_name virtual attr for user class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
 
   private
   def user_params
---
+++ spec/features/creating_a_reminder_feature_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+feature "creating a reminder" do 
+	let(:user) { User.create(	user_name: "dummy",
+														first_name: "dummy",
+														last_name: "user",
+														email: "dummy@dummy.dumb",
+														password: "12345678",
+														phone_number: "+16145551234")}
+	before(:each) do
+		visit root_path
+		click_link "Login"
+		fill_in "session_user_name", with: user.user_name
+		fill_in "session_password", with: user.password
+		click_button "Login"
+	end
+
+	scenario "user has logged in and has access to new reminder form" do
+		expect(page).to have_link("Create Reminder")
+	end
+
+	xscenario "create reminder button renders create reminder form" do
+		click_link "Create Reminder"
+		expect(view).to render_template("reminders/new")
+	end
+
+	xscenario "create reminder form contains ..." do
+		click_link "Create Reminder"
+	end
+	
+end
